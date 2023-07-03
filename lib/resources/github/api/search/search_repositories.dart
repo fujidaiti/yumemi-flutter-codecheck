31d189b6a3110114db9737e53d3bd2302a3df66b
fujidaiti <fujidaiti@gmail.com>
2023-07-04 03:43:01 +0900

    GitHubAPIを叩いて検索するモジュールを追加

---
+++ lib/resources/github/api/search/search_repositories.dart
@@ -0,0 +1,78 @@
+import 'package:meta/meta.dart';
+import 'package:yumemi_flutter_codecheck/resources/github/client.dart';
+
+// ドキュメント ↓
+// https://docs.github.com/en/rest/search/search?apiVersion=2022-11-28#search-repositories
+Future<SearchRepositoriesResult> searchRepositories({
+  required String query,
+  required int page,
+  required int perPage,
+}) async {
+  return parseResponse(
+    await GitHubApiClient().getJson(
+      endpoint: "/search/repositories",
+      queryParameters: {
+        "q": query,
+        "page": "$page",
+        "per_page": "$perPage",
+      },
+    ),
+  );
+}
+
+typedef SearchRepositoriesResult = ({
+  int totalCount,
+  List<_RepositoryOverview> items,
+});
+
+typedef _RepositoryOverview = ({
+  String name,
+  String owner,
+  String avatarUrl,
+  String? description,
+  String? language,
+  int stargazersCount,
+});
+
+@visibleForTesting
+SearchRepositoriesResult parseResponse(Map<String, dynamic> response) {
+  return switch (response) {
+    {
+      "total_count": int totalCount,
+      "items": List<dynamic> items,
+    } =>
+      (
+        totalCount: totalCount,
+        items: [...items.map(parseItem)],
+      ),
+    // TODO; 例外処理
+    _ => throw UnimplementedError("[search-repositories] 無効なデータ形式: $response"),
+  };
+}
+
+@visibleForTesting
+// ignore: library_private_types_in_public_api
+_RepositoryOverview parseItem(dynamic item) {
+  return switch (item) {
+    {
+      "name": String name,
+      "description": String? description,
+      "language": String? language,
+      "stargazers_count": int stargazersCount,
+      "owner": {
+        "login": String owner,
+        "avatar_url": String avatarUrl,
+      },
+    } =>
+      (
+        name: name,
+        owner: owner,
+        avatarUrl: avatarUrl,
+        language: language,
+        description: description,
+        stargazersCount: stargazersCount,
+      ),
+    // TODO; 例外処理
+    _ => throw UnimplementedError("[repository-overview] 無効なデータ形式 : $item"),
+  };
+}
