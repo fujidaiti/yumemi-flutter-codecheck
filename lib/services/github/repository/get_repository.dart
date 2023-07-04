cb96f75a14ed0a98379c226ada64333223b4c597
fujidaiti <fujidaiti@gmail.com>
2023-07-05 02:31:21 +0900

    リポジトリの詳細を取得する処理を実装

--- lib/services/github/repository/get_repository.dart
+++ lib/services/github/repository/get_repository.dart
@@ -1,18 +1,23 @@
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:yumemi_flutter_codecheck/resources/github/api/repositories/get_repository.dart';
 import 'package:yumemi_flutter_codecheck/services/github/repository/types/repository.dart';
 
 final repositoryProvider = FutureProvider.autoDispose.family(
-  (ref, ({String name, String owner}) params) {
-    return const Repository(
-      name: "Flutter",
-      owner: "Flutter",
-      avatarUrl: "https://avatars.githubusercontent.com/u/14101776?v=4",
-      description: "This is the offcial repository of Flutter project",
-      language: "Dart",
-      stargazersCount: 20000,
-      watchersCount: 100,
-      forksCount: 345,
-      openIssuesCount: 540,
+  (ref, ({String name, String owner}) params) async {
+    final result = await getRepository(
+      name: params.name,
+      owner: params.owner,
+    );
+    return Repository(
+      name: result.name,
+      owner: result.owner,
+      avatarUrl: result.avatarUrl,
+      description: result.description,
+      language: result.language,
+      stargazersCount: result.stargazersCount,
+      watchersCount: result.watchersCount,
+      forksCount: result.forksCount,
+      openIssuesCount: result.openIssuesCount,
     );
   },
 );
