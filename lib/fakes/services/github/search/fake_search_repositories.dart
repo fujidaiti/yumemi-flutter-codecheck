c2492c6afeb46ff0a9fe114e8efdf8fdb00eaaae
fujidaiti <fujidaiti@gmail.com>
2023-07-04 21:51:53 +0900

    fakerを導入

--- lib/fakes/services/github/search/fake_search_repositories.dart
+++ lib/fakes/services/github/search/fake_search_repositories.dart
@@ -1,10 +1,25 @@
-
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:yumemi_flutter_codecheck/fakes/services/github/faker.dart';
 import 'package:yumemi_flutter_codecheck/services/github/search/search_repositories.dart';
 import 'package:yumemi_flutter_codecheck/services/github/search/types/repository_overview.dart';
 
 final fakeRepositoriesProvider = FutureProvider.autoDispose.family(
   (ref, RepositoriesProviderParams params) async {
-    return (0, <RepositoryOverview>[]);
+    final totalCount = faker.randomGenerator.integer(100);
+    final items = List.generate(
+      totalCount,
+      (_) {
+        return RepositoryOverview(
+          name: faker.lorem.word(),
+          description: faker.lorem.sentence(),
+          owner: faker.lorem.word(),
+          avatarUrl: faker.internet.httpsUrl(),
+          language: faker.lorem.word(),
+          stars: faker.randomGenerator.integer(2000),
+        );
+      },
+    );
+
+    return (totalCount, items);
   },
 );
