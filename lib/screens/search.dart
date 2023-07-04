f57bb5bfe205a1d6f51e7a35913fa72298a38cf8
fujidaiti <fujidaiti@gmail.com>
2023-07-05 02:51:05 +0900

    再検索できるよう変更

--- lib/screens/search.dart
+++ lib/screens/search.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:go_router/go_router.dart';
-import 'package:yumemi_flutter_codecheck/components/github/repository_overview_list.dart';
 import 'package:yumemi_flutter_codecheck/components/common/search_box.dart';
+import 'package:yumemi_flutter_codecheck/components/github/repository_overview_list.dart';
+import 'package:yumemi_flutter_codecheck/services/github/search/types/repository_overview.dart';
 import 'package:yumemi_flutter_codecheck/services/github/search/types/search_query.dart';
 
-class Search extends StatelessWidget {
+class Search extends HookWidget {
   const Search({
     super.key,
     required this.query,
@@ -14,16 +16,29 @@ class Search extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final query = useState(this.query);
+
+    void onNewKeywordsSubmitted(String keywords) {
+      query.value = SearchQuery(
+        keywords: keywords.trim(),
+      );
+    }
+
+    void onTapItem(RepositoryOverview repo) {
+      context.push("/repository/${repo.owner}/${repo.name}");
+    }
+
     return Scaffold(
       appBar: AppBar(
         automaticallyImplyLeading: false,
-        title: const SearchBox(),
+        title: SearchBox(
+          initialText: query.value.keywords,
+          onSubmitted: onNewKeywordsSubmitted,
+        ),
       ),
       body: RepositoryOverviewList(
-        query: query,
-        onTapItem: (repo) {
-          context.push("/repository/${repo.owner}/${repo.name}");
-        },
+        query: query.value,
+        onTapItem: onTapItem,
         loadingBuilder: (context) =>
             const Center(child: CircularProgressIndicator()),
         errorBuilder: (context, error, stackTrace) {
