645449c4718c345ed19cc1c4d1af99ddb0bfda2b
fujidaiti <fujidaiti@gmail.com>
2023-07-05 15:44:52 +0900

    プルリクの数を取得する処理を実装

---
+++ lib/resources/github/api/search/search_pull_requests.dart
@@ -0,0 +1,36 @@
+import 'package:meta/meta.dart';
+import 'package:yumemi_flutter_codecheck/resources/github/client.dart';
+
+// ドキュメント ↓
+// https://docs.github.com/en/rest/search/search?apiVersion=2022-11-28#search-issues-and-pull-requests
+Future<SearchPullRequestsResult> searchPullRequests({
+  required String repository,
+  required String owner,
+  required bool isOpen,
+}) async {
+  final state = isOpen ? "open" : "closed";
+  //s earch/issues?q=is:pr+state:open+repo:flutter/flutter&per_page=1
+  return parseResponse(
+    await GitHubApiClient().getJson(
+      endpoint: "search/issues",
+      queryParameters: {
+        // 今の所検索結果の数だけ欲しいので`per_page=1`とする
+        "q": "is:pr state:$state repo:$owner/$repository",
+        "per_page": "1",
+      },
+    ),
+  );
+}
+
+typedef SearchPullRequestsResult = ({
+  int totalCount,
+});
+
+@visibleForTesting
+SearchPullRequestsResult parseResponse(Map<String, dynamic> response) {
+  return switch (response) {
+    {"total_count": int totalCount} => (totalCount: totalCount),
+    // TODO; 例外処理
+    _ => throw UnimplementedError("Unkown format: $response"),
+  };
+}
