4b9e4b3cc6f4bd9c254852945adb0c4e1adbd12e
fujidaiti <fujidaiti@gmail.com>
2023-07-06 23:14:57 +0900

    ルーターのエラー処理

--- lib/router.dart
+++ lib/router.dart
@@ -16,26 +16,17 @@ final router = GoRouter(
       builder: (context, state) {
         return switch (state.extra) {
           SearchQuery query => Search(query: query),
-          // TODO; 例外処理
-          _ => throw UnimplementedError("'/search'はディープリンクに未対応"),
+          _ => throw ArgumentError("'/search'はディープリンクに未対応"),
         };
       },
     ),
     GoRoute(
       path: "/repository/:owner/:name",
       builder: (context, state) {
-        return switch (state.pathParameters) {
-          {
-            "name": var name,
-            "owner": var owner,
-          } =>
-            RepositoryDetails(
-              name: name,
-              owner: owner,
-            ),
-          // TODO; 例外処理
-          _ => throw UnimplementedError(),
-        };
+        return RepositoryDetails(
+          name: state.pathParameters["name"]!,
+          owner: state.pathParameters["woner"]!,
+        );
       },
     ),
   ],
