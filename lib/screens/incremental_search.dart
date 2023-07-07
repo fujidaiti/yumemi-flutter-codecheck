a3659caa2839513c31fa6a942040d98bbeb658b8
fujidaiti <fujidaiti@gmail.com>
2023-07-08 01:17:04 +0900

    インクリメンタルサーチ画面を実装

--- lib/screens/incremental_search.dart
+++ lib/screens/incremental_search.dart
@@ -1,36 +1,38 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:flutter_octicons/flutter_octicons.dart';
 import 'package:go_router/go_router.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:yumemi_flutter_codecheck/common/errors/types/request_aborted_exception.dart';
 import 'package:yumemi_flutter_codecheck/common/errors/unkown_error_widget.dart';
 import 'package:yumemi_flutter_codecheck/components/common/search_box.dart';
-import 'package:yumemi_flutter_codecheck/components/github/repository_overview_list.dart';
+import 'package:yumemi_flutter_codecheck/services/github/search/search_repositories.dart';
 import 'package:yumemi_flutter_codecheck/services/github/search/types/repository_overview.dart';
 import 'package:yumemi_flutter_codecheck/services/github/search/types/search_query.dart';
 
 class IncrementalSearch extends HookWidget {
   const IncrementalSearch({
     super.key,
-    required this.query,
+    required this.initialQuery,
   });
 
-  final SearchQuery query;
+  final SearchQuery initialQuery;
 
   @override
   Widget build(BuildContext context) {
-    final query = useState(this.query);
-
-    void onKeywordsSubmitted(String keywords) {
-      query.value = SearchQuery(
-        keywords: keywords.trim(),
-      );
-    }
+    final query = useValueNotifier(initialQuery);
 
     void onKeywordsChanged(String keywords) {
       query.value = SearchQuery(keywords: keywords.trim());
     }
 
-    void onTapItem(RepositoryOverview repo) {
-      context.push("/repository/${repo.owner}/${repo.name}");
+    void onKeywordsSubmitted(String _) {
+      if (query.value.keywords.isNotEmpty) {
+        context
+            .push("/search", extra: query.value)
+            // 検索画面と一緒にこの画面もpopする
+            .then((_) => Navigator.of(context).pop());
+      }
     }
 
     return Scaffold(
@@ -43,26 +45,100 @@ class IncrementalSearch extends HookWidget {
           onTextChanged: onKeywordsChanged,
         ),
       ),
-      body: RepositoryOverviewList(
-        query: query.value,
-        onTapItem: onTapItem,
-        loadingBuilder: (context) =>
-            const Center(child: CircularProgressIndicator()),
-        errorBuilder: (context, error, stackTrace) {
-          return UnkownErrorWidget(error, stackTrace);
+      body: ValueListenableBuilder(
+        valueListenable: query,
+        builder: (context, query, _) {
+          return _SearchResultList(query);
         },
       ),
     );
   }
 
-  static void show(BuildContext context) {
+  static void showAsDialog(BuildContext context, SearchQuery query) {
     Navigator.of(context).push(
       MaterialPageRoute(
         fullscreenDialog: true,
         builder: (context) {
-          return const IncrementalSearch(query: SearchQuery(keywords: ""));
+          return IncrementalSearch(
+            initialQuery: query,
+          );
         },
       ),
+    );
+  }
+}
+
+final _incrementalSearchResult = FutureProvider.autoDispose.family(
+  (ref, SearchQuery query) async {
+    // 最初の`maxItemCount`件だけ取得する
+    const maxItemCount = 6;
+    const debounceDuration = Duration(milliseconds: 500);
+
+    if (query.keywords.isEmpty) {
+      return const <RepositoryOverview>[];
+    }
+
+    bool isCanceled = false;
+    ref.onDispose(() => isCanceled = true);
+    await Future.delayed(debounceDuration);
+    if (isCanceled) {
+      throw const RequestAbortedException("_incrementalSearchResult");
+    }
+
+    final params = (page: 1, perPage: maxItemCount, query: query);
+    final (_, items) = await ref.watch(repositoriesProvider(params).future);
+    return items;
+  },
+);
+
+class _SearchResultList extends ConsumerWidget {
+  const _SearchResultList(this.query);
+
+  final SearchQuery query;
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return ref.watch(_incrementalSearchResult(query)).when(
+          data: _build,
+          error: UnkownErrorWidget.new,
+          loading: _buildLoading,
+        );
+  }
+
+  Widget _build(List<RepositoryOverview> items) {
+    return ListView.builder(
+      itemCount: items.length,
+      itemBuilder: (context, index) {
+        return _SearchResultItem(items[index]);
+      },
+    );
+  }
+
+  Widget _buildLoading() {
+    return const Center(child: CircularProgressIndicator());
+  }
+}
+
+class _SearchResultItem extends StatelessWidget {
+  const _SearchResultItem(this.repo);
+
+  final RepositoryOverview repo;
+
+  @override
+  Widget build(BuildContext context) {
+    void onTapItem() {
+      context.push("/repository/${repo.owner}/${repo.name}");
+    }
+
+    return ListTile(
+      onTap: onTapItem,
+      leading: const Icon(OctIcons.repo_24),
+      title: Text(
+        "${repo.owner}/${repo.name}",
+        maxLines: 1,
+        overflow: TextOverflow.ellipsis,
+      ),
+      trailing: const Icon(OctIcons.arrow_up_right_16),
     );
   }
 }
