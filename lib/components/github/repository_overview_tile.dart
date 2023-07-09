93abb83284807c980ebe7b946cda4322dbf223f1
fujidaiti <fujidaiti@gmail.com>
2023-07-09 17:10:41 +0900

    リポジトリ概要タイルのレイアウト調整

--- lib/components/github/repository_overview_tile.dart
+++ lib/components/github/repository_overview_tile.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_octicons/flutter_octicons.dart';
+import 'package:github_language_colors/github_language_colors.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:yumemi_flutter_codecheck/common/errors/unkown_error_widget.dart';
+import 'package:yumemi_flutter_codecheck/components/common/avatar_icon.dart';
 import 'package:yumemi_flutter_codecheck/services/github/search/types/repository_overview.dart';
 
-const _minTileHeight = 160.0;
+const _minTileHeight = 140.0;
+const _fallbackLanguageColor = Colors.lightGreen;
 
 typedef OnTapRepositoryOverviewTileCallback = void Function(
   RepositoryOverview rpeo,
@@ -39,35 +42,91 @@ class RepositoryOverviewTile extends StatelessWidget {
           constraints: const BoxConstraints(
             minHeight: _minTileHeight,
           ),
-          child: Padding(
-            padding: const EdgeInsets.symmetric(
-              horizontal: 20,
-              vertical: 12,
-            ),
-            child: body,
-          ),
+          child: body,
         ),
       ),
     );
   }
 
-  Widget _build(BuildContext context, RepositoryOverview overview) {
-    return Column(
+  Widget _build(BuildContext context, RepositoryOverview repo) {
+    final header = Row(
       children: [
-        Text(
-          overview.owner,
-          style: Theme.of(context).textTheme.titleSmall,
+        AvatarIcon(
+          url: repo.avatarUrl,
+          size: 24,
         ),
+        const SizedBox(width: 12),
         Text(
-          overview.name,
-          style: Theme.of(context).textTheme.titleLarge,
+          repo.owner,
+          style: Theme.of(context).textTheme.titleMedium?.copyWith(
+                fontWeight: FontWeight.normal,
+              ),
+          maxLines: 1,
+          overflow: TextOverflow.ellipsis,
         ),
-        if (overview.description != null)
-          Text(
-            overview.description!,
-            style: Theme.of(context).textTheme.bodyLarge,
-          ),
       ],
+    );
+
+    final name = Text(
+      repo.name,
+      style: Theme.of(context).textTheme.titleMedium?.copyWith(
+            fontWeight: FontWeight.bold,
+          ),
+    );
+
+    final description = switch (repo.description) {
+      String text when text.isNotEmpty => Text(
+          text,
+          style: Theme.of(context).textTheme.bodyLarge,
+        ),
+      _ => const SizedBox(height: 20),
+    };
+
+    final stars = Chip(
+      label: Text("${repo.stars}"),
+      avatar: const Icon(OctIcons.star_16),
+      visualDensity: VisualDensity.compact,
+    );
+
+    final language = switch (repo.language) {
+      null => null,
+      String name => Chip(
+          label: Text(name),
+          avatar: Icon(
+            Icons.circle,
+            color: switch (githubLanguageColors[name]) {
+              int hexCode => Color(hexCode),
+              null => _fallbackLanguageColor,
+            },
+          ),
+          visualDensity: VisualDensity.compact,
+        ),
+    };
+
+    final badges = Row(
+      children: [
+        stars,
+        if (language != null) ...[
+          const SizedBox(width: 10),
+          language,
+        ],
+      ],
+    );
+
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(16, 16, 24, 16),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          header,
+          const SizedBox(height: 4),
+          name,
+          const SizedBox(height: 4),
+          description,
+          const SizedBox(height: 8),
+          badges,
+        ],
+      ),
     );
   }
 
