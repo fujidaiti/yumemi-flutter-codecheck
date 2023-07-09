f804e4b53683ebdc7d576f0d35d9c9c8487ba102
fujidaiti <fujidaiti@gmail.com>
2023-07-09 14:04:52 +0900

    フェイクデータを改善

--- lib/fakes/services/github/repository/fake_get_repository.dart
+++ lib/fakes/services/github/repository/fake_get_repository.dart
@@ -1,3 +1,4 @@
+import 'package:github_language_colors/github_language_colors.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:yumemi_flutter_codecheck/common/values/percentage.dart';
 import 'package:yumemi_flutter_codecheck/fakes/services/github/faker.dart';
@@ -5,6 +6,7 @@ import 'package:yumemi_flutter_codecheck/services/github/repository/types/reposi
 
 final fakeRepositoryProvider = FutureProvider.autoDispose.family(
   (ref, ({String name, String owner}) params) async {
+    // 遅延
     await Future.delayed(const Duration(milliseconds: 750));
     return Repository(
       name: params.name,
@@ -18,12 +20,9 @@ final fakeRepositoryProvider = FutureProvider.autoDispose.family(
       openIssuesCount: faker.randomGenerator.integer(200),
       openPullRequestsCount: faker.randomGenerator.integer(200),
       languages: {
-        "C": Percentage(40),
-        "Dart": Percentage(20),
-        "Swift": Percentage(10),
-        "Python": Percentage(20),
-        "Shell": Percentage(7.5),
-        "List": Percentage(2.5),
+        for (var i = 0; i < faker.randomGenerator.integer(7); ++i)
+          faker.randomGenerator.element(githubLanguageColors.keys.toList()):
+              Percentage(faker.randomGenerator.decimal(scale: 30.0, min: 0.1)),
       },
     );
   },
