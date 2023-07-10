988d4922c09ae63f12a0281dbfa8e5f4626a9729
fujidaiti <fujidaiti@users.noreply.github.com>
2023-07-10 00:30:09 +0000

    deploy: 638201f54c68650c2059eaf7bda4bcd3dcb767e3

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"index.html": "376454c09177408fdb13cb6e46d41352",
-"/": "376454c09177408fdb13cb6e46d41352",
+const RESOURCES = {"index.html": "0e102472615159d25309939a693b0508",
+"/": "0e102472615159d25309939a693b0508",
 "flutter.js": "6fef97aeca90b426343ba6c5c9dc5d4a",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
-"main.dart.js": "2ec1e176ba37cd191f42f5056b50978e",
+"main.dart.js": "f893a05e0ec75ac2c27017557b02284d",
 "version.json": "382f57e6c85150300ce245b8effbef5a",
 "canvaskit/chromium/canvaskit.js": "8c8392ce4a4364cbb240aa09b5652e05",
 "canvaskit/chromium/canvaskit.wasm": "fc18c3010856029414b70cae1afc5cd9",
